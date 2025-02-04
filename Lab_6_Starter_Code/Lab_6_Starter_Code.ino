84112b09a216edf1963b333cdf23911bd42a84f5
epeterson02 <erikpeterson335@gmail.com>
2025-02-04 14:34:57 -0600

    Update Lab_6_Starter_Code.ino
    Co-Authored-By: Dizzy <54298425+dizzyfop@users.noreply.github.com>

--- Lab_6_Starter_Code/Lab_6_Starter_Code.ino
+++ Lab_6_Starter_Code/Lab_6_Starter_Code.ino
@@ -53,12 +53,9 @@ void loop() {
   String dataString = "";
 
   // read three sensors and append to the string:
-  for (int analogPin = 0; analogPin < 3; analogPin++) {
-    int sensor = analogRead(analogPin);
+  //for (int analogPin = 0; analogPin < 3; analogPin++) {
+    int sensor = analogRead(A2);
     dataString += String(sensor);
-    if (analogPin < 2) {
-      dataString += ",";
-    }
   }
 
   // open the file. note that only one file can be open at a time,
